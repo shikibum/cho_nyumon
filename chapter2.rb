52d84fac9695740b4f2fd804e20d5e39b98b55f9
Rina Mito <satoshikibu@gmail.com>
2019-01-15 16:38:04 +0900

    capter2

--- chapter2.rb
+++ chapter2.rb
@@ -1,3 +1,25 @@
 # 2-1
 puts 2 + 3
 2 * 2 * 3.14
+
+# 2-2
+puts 'ruby'
+puts 'abc' + 'def'
+puts '123'.to_i + '456'.to_i
+
+# 2-3
+coffee = 300
+espresso = 100
+puts "コーヒー: #{coffee}円"
+puts "合計: #{coffee + espresso * 2}"
+coffee = 400
+
+# 2-6
+total = 300 * 2
+p total
+tax = total * 1.08
+puts tax
+
+# 2-7
+total = 300 * 2
+puts total
